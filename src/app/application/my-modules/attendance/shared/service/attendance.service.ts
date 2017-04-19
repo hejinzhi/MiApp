d217d94606a1d1b9e35e05bb5077062893d001ff
Garyhjj <379751452@qq.com>
2017-04-19 15:04:06 +0800

    add service

--- src/app/application/my-modules/attendance/shared/service/attendance.service.ts
+++ src/app/application/my-modules/attendance/shared/service/attendance.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@angular/core';
 import { MyHttpService } from '../../../../../core/services/myHttp.service';
+import { MyFormModel } from '../models/my-form.model'
+
 @Injectable()
 export class AttendanceService {
   constructor(private myHttp: MyHttpService) {  }
+
+  getSelectType(type:string){
+    return this.myHttp.post('',{type:type}).then((res) => {
+      return Promise.resolve(res.json())
+    });
+  }
+
+  submit(formData:MyFormModel) {
+    return this.myHttp.post('',formData).then((res) => {
+      return Promise.resolve(res.json())
+    });
+  }
+
+  save(formData:MyFormModel) {
+    return this.myHttp.post('',formData).then((res) => {
+      return Promise.resolve(res.json())
+    });
+  }
+
+  cancel(formData:MyFormModel) {
+    return this.myHttp.post('',formData).then((res) => {
+      return Promise.resolve(res.json())
+    });
+  }
+
+  callback(formData:MyFormModel) {
+    return this.myHttp.post('',formData).then((res) => {
+      return Promise.resolve(res.json())
+    });
+  }
+
+  getForm(type:string) {
+    return this.myHttp.post('',{type:type}).then((res) => {
+      return Promise.resolve(res.json())
+    });
+  }
 }
