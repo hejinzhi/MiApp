adb55c9a5472d229e1c967acfd89b266f1c6ab8c
Garyhjj <379751452@qq.com>
2017-04-27 13:29:14 +0800

    change the logic of the android backAction

--- src/app/application/my-modules/attendance/shared/config/attendance.config.ts
+++ src/app/application/my-modules/attendance/shared/config/attendance.config.ts
@@ -5,4 +5,7 @@ export class AttendanceConfig {
     // 根据模块获取function清单
     static getSignListUrl = EnvConfig.baseUrl + 'Att/GetApproveList?docNum=';
 
+    // 根据模块获取function清单
+    static saveLeaveUrl = EnvConfig.baseUrl + 'OffDuty/AddOffDuty';
+
 }
