6b6471f7ad8486a1e94d9dfdd3b23a08781186e8
Garyhjj <379751452@qq.com>
2017-06-06 15:48:03 +0800

    change setting

--- src/app/shared/config/env.config.ts
+++ src/app/shared/config/env.config.ts
@@ -1,6 +1,6 @@
 export class EnvConfig {
     // static baseUrl: string = 'http://10.86.118.81/webapi/';
-    static baseUrl: string = 'http://10.86.16.166/webapi/';
+    static baseUrl: string = 'http://webapi.mic.com.cn/webapi/';
 
     static companyID: string = 'MSL';
 }
