0e607e93567ecaf484cedb0cc6b49715bf055c38
jinzhi <504151911@qq.com>
2017-06-16 10:19:04 +0800

    change id address

--- src/app/shared/config/env.config.ts
+++ src/app/shared/config/env.config.ts
@@ -1,8 +1,8 @@
 export class EnvConfig {
 
-    static baseUrl: string = 'http://10.86.16.166/webapi/';
+    // static baseUrl: string = 'http://10.86.16.166/webapi/';
     // static baseUrl: string = 'http://webapi.mic.com.cn/webapi/';
-    // static baseUrl: string = 'http://218.13.31.10/webapi/';
+    static baseUrl: string = 'http://218.13.31.10/webapi/';
     static companyID: string = 'MSL';
     static maxGrade: number = 1;  // 记录该公司最高级部门的等级,如顺达是1
 }
