bd4663627b9b58cc7f5fc5f0e32115424eaac047
jinzhi <504151911@qq.com>
2017-06-19 14:22:09 +0800

    add pipe to app.component

---
+++ src/app/tabs/shared/config/language.config.ts
@@ -0,0 +1,17 @@
+export class LanguageConfig {
+    static tabComponent = {
+        traditional_Chinese: {
+            message: '消息',
+            application: '應用',
+            contact: '通訊錄',
+            me: '我'
+        },
+
+        simple_Chinese: {
+            message: '消息',
+            application: '应用',
+            contact: '通讯录',
+            me: '我'
+        }
+    };
+}
