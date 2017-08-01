939a9380d54816d281a5b28e3db212d5682bc0e6
cqzxliang <448945892@qq.com>
2017-08-01 13:30:04 +0800

    add voice

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -117,6 +117,7 @@ export class MessageComponent implements OnInit {
   async handleReceiveMessageAndroid(res: any) {
     let _content: string;
     let child_type: string;
+    let vounread: string = 'N';
 
     if (res.contentType === 'text') {
       _content = res.content.text;
@@ -124,6 +125,7 @@ export class MessageComponent implements OnInit {
       _content = res.content.localThumbnailPath;
     } else if (res.contentType === 'voice') {
       _content = res.content.local_path;
+      vounread = 'Y';
     }
 
     if (res.fromName === 'signlist' || res.fromName === 'news' || res.fromName === 'alert' || res.fromName === 'report') {
@@ -150,11 +152,13 @@ export class MessageComponent implements OnInit {
       type: this._type,
       unread: true,
       imageHeight: res.content.height,
-      imageWidth: res.content.width
+      imageWidth: res.content.width,
+      duration: res.content.duration,
+      vounread: vounread
     };
 
     await this.databaseService.addMessage(res.targetInfo.userName, res.fromName, this.userinfo.username, _content, res.contentType, res.createTimeInMillis, this._type, 'Y',
-      JSON.stringify(res.content.extras), child_type, res.content.height, res.content.width);
+      JSON.stringify(res.content.extras), child_type, res.content.height, res.content.width, res.content.duration, vounread);
 
     return msg;
   }
@@ -162,12 +166,17 @@ export class MessageComponent implements OnInit {
   async handleReceiveMessageIos(res: any) {
     let _content: string;
     let child_type: string;
+    let vounread: string = 'N';
     if (res.content.msg_type === 'text') {
       _content = res.content.msg_body.text;
     } else if (res.content.msg_type === 'image') {
       let tempStr: string = res.resourcePath;
       tempStr = tempStr.replace('large', 'thumb');
       _content = tempStr;
+    } else if (res.content.msg_type === 'voice') {
+      //要添加IOS聲音地址
+      // _content = res.content.msg_body.local_path;
+      vounread = 'Y';
     }
 
     if (res.content.from_id === 'signlist' || res.content.from_id === 'news' || res.content.from_id === 'alert' || res.content.from_id === 'report') {
@@ -193,16 +202,17 @@ export class MessageComponent implements OnInit {
       type: this._type,
       unread: true,
       imageHeight: res.content.msg_body.height,
-      imageWidth: res.content.msg_body.width
+      imageWidth: res.content.msg_body.width,
+      duration: res.content.msg_body.duration,
+      vounread: vounread
     };
 
     await this.databaseService.addMessage(res.content.target_id, res.content.from_id, this.userinfo.username, _content, res.content.msg_type, res.content.create_time, this._type, 'Y',
-      JSON.stringify(res.content.msg_body.extras), child_type, res.content.msg_body.height, res.content.msg_body.width);
+      JSON.stringify(res.content.msg_body.extras), child_type, res.content.msg_body.height, res.content.msg_body.width, res.content.msg_body.duration, vounread);
 
     return msg;
 
   }
-
 
 
   async refreshData() {
