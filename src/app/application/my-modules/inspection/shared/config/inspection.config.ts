faf28360474a9a6d75e5844714c2fee91e35a934
jinzhi <504151911@qq.com>
2017-09-05 09:06:40 +0800

    change json server

--- src/app/application/my-modules/inspection/shared/config/inspection.config.ts
+++ src/app/application/my-modules/inspection/shared/config/inspection.config.ts
@@ -1,5 +1,5 @@
 // import { EnvConfig } from './../../../../../shared/config/env.config';
-let baseUrl: string = 'http://localhost:3000/';
+let baseUrl: string = 'http://10.86.13.20:3000/';
 
 export class InspectionConfig {
     static getNamesUrl = baseUrl + 'name';
