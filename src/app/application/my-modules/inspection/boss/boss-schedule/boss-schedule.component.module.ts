5af89d02a0d911f2c782c00cb5941d46ccaffd4d
cqzxliang <448945892@qq.com>
2017-09-14 10:56:02 +0800

    添加頁面

---
+++ src/app/application/my-modules/inspection/boss/boss-schedule/boss-schedule.component.module.ts
@@ -0,0 +1,20 @@
+import { PhotoViewComponentModule } from './../../../../../shared/components/photo-view/photo-view.component.module';
+import { BossScheduleComponent } from './boss-schedule.component';
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+    imports: [
+        IonicPageModule.forChild(BossScheduleComponent),
+        TranslateModule.forChild(),
+        PhotoViewComponentModule
+    ],
+    exports: [],
+    declarations: [BossScheduleComponent],
+    providers: [],
+    entryComponents: [
+        BossScheduleComponent
+    ]
+})
+export class BossReportComponentModule { }
