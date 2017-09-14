963de33e9642fe702b469460b17a8ce09fc5c9d3
Garyhjj <379751452@qq.com>
2017-09-14 13:51:26 +0800

    improve

--- src/app/application/my-modules/inspection/shared/page/admin-check/admin-check.component.module.ts
+++ src/app/application/my-modules/inspection/shared/page/admin-check/admin-check.component.module.ts
@@ -1,3 +1,5 @@
+import { EquipItemComponentModule } from './../../component/equip-item/equip-item.component.module';
+import { BossItemComponentModule } from './../../component/boss-item/boss-item.component.module';
 import { AdminCheckComponent } from './admin-check.component';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
@@ -7,6 +9,8 @@ import { TranslateModule } from '@ngx-translate/core';
     imports: [
         IonicPageModule.forChild(AdminCheckComponent),
         TranslateModule.forChild(),
+        BossItemComponentModule,
+        EquipItemComponentModule
     ],
     exports: [],
     declarations: [AdminCheckComponent],
--- src/app/application/my-modules/inspection/shared/page/admin-check/admin-check.component.ts
+++ src/app/application/my-modules/inspection/shared/page/admin-check/admin-check.component.ts
@@ -1,4 +1,4 @@
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, NavParams, NavController } from 'ionic-angular';
 import { Component, OnInit } from '@angular/core';
 
 @IonicPage()
@@ -7,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'admin-check.component.html'
 })
 export class AdminCheckComponent implements OnInit {
-
-  constructor() { }
+  statsList = ['待处理', '已处理', '待分配', 'HighLight']
+  type: number = 0;
+  status: string = '5';
+  testData1 = [{
+    time: '2017-05-09 17:20', name: '小东', site: '楼递间', detail: '有垃圾', status: '待处理', inCharge: '小天'
+  },
+  {
+    time: '2017-05-09 17:22', name: '小东', site: '楼递间', detail: '有垃圾', status: '已处理', inCharge: '小天1'
+  },
+  {
+    time: '2017-05-09 17:23', name: '小东', site: '楼递间', detail: '有垃圾', status: '待分配', inCharge: '小天2'
+  },
+  {
+    time: '2017-05-09 17:24', name: '小东', site: '楼递间', detail: '有垃圾', status: 'HighLight', inCharge: '小天3'
+  }
+  ]
+  testData2 = [{
+    time: '2017-05-09 17:25', name: '小东', site: '楼递间', detail: '过期', equip_num: '123456', issue: '是否过期', status: '已处理', inCharge: '小天'
+  },
+  {
+    time: '2017-05-09 17:26', name: '小东', site: '楼递间', detail: '过期', equip_num: '123456', issue: '是否过期', status: '待处理', inCharge: '小天'
+  }]
+  testData: any[];
+  constructor(
+    private navParams: NavParams,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+    this.type = this.navParams.get('type') || 0;
+    this.initData();
   }
+  initData() {
+    if (this.type === 1) {
+      this.testData = this.testData1.slice();
+    } else if (this.type === 2) {
+      this.testData = this.testData2.slice();
+    }
+  }
+
+  changeShow() {
+    this.initData();
+    console.log(this.status);
+    if(+this.status < 5) {
+      this.testData = this.testData.filter((item) => item.status === this.statsList[+this.status-1])
+    }
+  }
+
+  toDetail(item: any) {
+    console.log(item);
+    this.navCtrl.push('IssueDetailComponent', {
+        issue: item,
+        type: this.type
+    })
+}
 
 }
